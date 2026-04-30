825ddeef7b00ead8f8baeb9f14a7a90395e5538d
Felix Karlsson <sout.coding@gmail.com>
2026-04-30 21:41:45 +0200

    Added methods for accessing user requests

--- src/main/java/com/app/oslotoilet/contribution/ContributionController.java
+++ src/main/java/com/app/oslotoilet/contribution/ContributionController.java
@@ -2,35 +2,52 @@ package com.app.oslotoilet.contribution;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import tools.jackson.databind.json.JsonMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/contribution")
 public class ContributionController {
 
     private final ContributionService contributionService;
-    private final JsonMapper.Builder builder;
 
-    public ContributionController(ContributionService contributionService, JsonMapper.Builder builder){
+    public ContributionController(ContributionService contributionService){
         this.contributionService = contributionService;
-        this.builder = builder;
     }
 
 
-    public ResponseEntity<Void> createNewLocationRequest(LocationRequest locationRequest){
+    @GetMapping
+    public List<LocationRequest> getRequests(@RequestParam(required = false) RequestStatus status){
+        if (status != null){
+            return contributionService.findByRequestStatus(status);
+        }
+        return contributionService.getAllRequests();
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<Void> createNewLocationRequest(@RequestBody LocationRequest locationRequest){
         //TODO: check https status codes, change to correct
         if (contributionService.createNewLocationRequest(locationRequest)){
-            return ResponseEntity.ok().build();
+            return ResponseEntity.status(201).build();
         }
         return ResponseEntity.badRequest().build();
     }
 
-    public ResponseEntity<LocationRequest> deleteLocationRequest(LocationRequest locationRequest){
-        if (contributionService.deleteLocationRequest(locationRequest)){
-            return ResponseEntity.ok().build();
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLocationRequestbyId(@PathVariable UUID id){
+        if (contributionService.deleteLocationRequestById(id)){
+            return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{userId}")
+    public List<LocationRequest> getRequestsbyUser(@PathVariable UUID userId, @RequestParam(required = false) RequestStatus status){
+        if (status != null){
+            return contributionService.findByuserIdAndRequestStatus(userId, status);
+        }
+        return contributionService.findByUserIdOrderByCreatedAtDesc(userId);
     }
 }
--- src/main/java/com/app/oslotoilet/contribution/ContributionService.java
+++ src/main/java/com/app/oslotoilet/contribution/ContributionService.java
@@ -2,6 +2,9 @@ package com.app.oslotoilet.contribution;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class ContributionService {
 
@@ -11,17 +14,36 @@ public class ContributionService {
         this.contributionRepository = contributionRepository;
     }
 
-
     public boolean createNewLocationRequest(LocationRequest locationRequest){
         //TODO: Add business logic
         contributionRepository.save(locationRequest);
         return true;
     }
 
-    public boolean deleteLocationRequest(LocationRequest locationRequest){
-        contributionRepository.delete(locationRequest);
-        return true;
+    public boolean deleteLocationRequestById(UUID id){
+        if (contributionRepository.existsById(id)){
+            contributionRepository.deleteById(id);
+            return true;
+        }
+
+        return false;
     }
+
+    public List<LocationRequest> getAllRequests(){
+        return contributionRepository.findAll();
+    }
+
+    public List<LocationRequest> findByRequestStatus(RequestStatus requestStatus){
+        return contributionRepository.findByRequestStatus(requestStatus);
+    }
+    public List<LocationRequest> findByuserIdAndRequestStatus(UUID userId, RequestStatus requestStatus){
+        return contributionRepository.findByuserIdAndRequestStatus(userId, requestStatus);
+    }
+
+    public List<LocationRequest> findByUserIdOrderByCreatedAtDesc(UUID userId){
+        return contributionRepository.findByUserIdOrderByCreatedAtDesc(userId);
+    }
+
 
 
 
