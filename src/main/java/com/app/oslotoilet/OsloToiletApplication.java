0ffa8cb46577f0075ecfaab37c3186055576a1b0
Felix Karlsson <sout.coding@gmail.com>
2026-04-30 19:57:14 +0200

    initial commit

---
+++ src/main/java/com/app/oslotoilet/OsloToiletApplication.java
@@ -0,0 +1,13 @@
+package com.app.oslotoilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsloToiletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsloToiletApplication.class, args);
+    }
+
+}
